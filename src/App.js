02b6110b79363795bd148253656f60d6ba391810
Tomasz Lewandowski <tomek2x1@wp.pl>
2021-11-22 17:02:31 +0100

    poprawa wygladu formularza

--- src/App.js
+++ src/App.js
@@ -10,6 +10,27 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 
+import { styled } from '@mui/styles';
+
+const MyButton = styled(Button)({ 
+  background:"linear-gradient(to right, #f07d3b, #ea5036)",
+  marginTop:30,
+  marginBottom:30,
+});;
+
+const MyCard = styled(Card)({ 
+  border: '2px solid transparent',
+  borderRadius:20,
+  backgroundImage: 'linear-gradient(white, white), radial-gradient(circle at top left, #f07d3b,#ea5036)',
+  backgroundOrigin: 'border-box',
+  backgroundClip: 'content-box, border-box',
+});
+
+const Form = styled("form")({ 
+  marginLeft:30,
+  marginRight:30,
+});
+
 const App = () => {
   const [data, setData] = useState({
     name:"",
@@ -34,32 +55,33 @@ const App = () => {
   return (
     <div className="App">
       <Box sx={{ maxWidth: 600, margin: "50px auto"}}>
-        <Card variant="outlined">
+        <MyCard variant="outlined">
           <h1>Generator stopek</h1>
-          <form>
-            <TextField id="name" name="name" value={data.name} label="Imię i nazwisko" variant="outlined" fullWidth margin="dense" onChange={e=>handleChange(e)}/>
-            <TextField id="position" name="position" value={data.position} label="Stanowisko" variant="outlined" fullWidth margin="dense" onChange={e=>handleChange(e)}/>
-            <TextField id="phone" name="phone" value={data.phone} label="Numer telefonu" variant="outlined" fullWidth margin="dense" onChange={e=>handleChange(e)}/>
-            <TextField id="email" name="email" value={data.email} label="Adres email" variant="outlined" fullWidth margin="dense" onChange={e=>handleChange(e)}/>
+          <Form>
+            <TextField id="name" name="name" value={data.name} label="Imię i nazwisko" variant="outlined" fullWidth margin="dense" size="small" onChange={e=>handleChange(e)}/>
+            <TextField id="position" name="position" value={data.position} label="Stanowisko" variant="outlined" fullWidth margin="dense" size="small" onChange={e=>handleChange(e)}/>
+            <TextField id="phone" name="phone" value={data.phone} label="Numer telefonu" variant="outlined" fullWidth margin="dense" size="small" onChange={e=>handleChange(e)}/>
+            <TextField id="email" name="email" value={data.email} label="Adres email" variant="outlined" fullWidth margin="dense" size="small" onChange={e=>handleChange(e)}/>
             <FormControl fullWidth>
-              <InputLabel id="workplace-label">Age</InputLabel>
+              <InputLabel id="workplace-label" size="small">Miejsce pracy</InputLabel>
               <Select
                 labelId="workplace-label"
                 id="workplace"
-                label="Age"
+                label="Miejsce pracy"
                 variant="outlined"
                 name="workplace"
                 value={data.workplace}
                 onChange={e=>handleChange(e)}
+                size="small"
               >
                 {workspace}
               </Select>
             </FormControl>
-            <Button variant="contained" endIcon={<SendIcon />}>
+            <MyButton variant="contained" endIcon={<SendIcon />}>
               Generuj
-            </Button>
-          </form>
-        </Card>
+            </MyButton>
+          </Form>
+        </MyCard>
       </Box>
     </div>
   );
